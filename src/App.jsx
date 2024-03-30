40e3eaf98e409dc3647800bfb8c6533a12ed9898
Sarwar Hossain <sarwarhridoy4@gmail.com>
2024-03-30 17:39:33 +0600

    ongoing...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useTranslation } from 'react-i18next';
 import './App.css'
 
 const App = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div className="container">
-      <h2>Hello World!</h2>
+      <h2>{t('Welcome to React')}</h2>
     </div>
   );
 };
